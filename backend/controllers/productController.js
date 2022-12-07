91998f6801698140e99cd59a9f604d1c1e58d1b1
AbdullahSharif <abdullah.29.umar@gmail.com>
2022-12-07 18:04:14 +0500

    added the routes for updation and deletion of products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,3 +19,42 @@ exports.createProduct = async function (req, res, next) {
     })
 
 }
+
+// update a product - Admin Route
+exports.updateProduct = async function (req, res, next) {
+    // first check if the product exists the product.
+    let product = await Product.findById(req.params.id);
+    if (!product) return res.status(400).json({
+        success: false,
+        message: "Product not found!"
+    })
+
+    // if the product is found then update it.
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+    res.status(200).json({
+        success: true,
+        product
+    });
+
+}
+
+// delete a product -- Admin route
+exports.deleteProduct = async function (req, res, next) {
+    // check if the product exists or not.
+    const product = await Product.findById(req.params.id);
+
+    if (!product) return res.status(400).json({
+        success: false,
+        message: "No such product exists!"
+    });
+
+    await product.remove();
+    res.status(200).json({
+        success: true,
+        message: "Deleted Successfully!"
+    });
+}

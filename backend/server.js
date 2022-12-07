4753b434bd442e9d91cfa84229ccb24fc13dea4d
AbdullahSharif <abdullah.29.umar@gmail.com>
2022-12-07 21:45:42 +0500

    handeled 4th type of error : any uncaught exception

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,14 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const dbConnection = require("./config/database");
 
+// if any uncaught exceptions occur
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to uncaught exception.");
+    process.exit(1);
+
+})
+
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT;
 

f37e4f7785f4a6ea3b76bb6311e0504ef92024ef
AbdullahSharif <abdullah.29.umar@gmail.com>
2023-02-10 22:02:29 +0500

    forgot password functionality added

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const sendToken = require("../utils/jwtToken");
 const User = require("../models/userModel");
+const sendEmail = require("../utils/sendEmail.js");
 
 // register a user.
 
@@ -55,4 +56,50 @@ exports.logoutUser = catchAsyncErrors(async (req, res, user) => {
         success: true,
         message: "Logged out!"
     })
+})
+
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+    // find user.
+    const user = await User.findOne({ email: req.body.email });
+
+    // if user not found.
+    if (!user) {
+        return next(new ErrorHandler("No such user exists!"));
+    }
+
+    // if user exists, then generate a password reset token for the user.
+    const token = user.setResetPasswordToken();
+
+    // now when you have generated the token for the user and set it in the document.
+    // save iy.
+    await user.save();
+
+    // now we preapre the email, which we have to send to the user to reset the password.
+    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/allRoutes/password/reset/${token}`;
+
+    const message = `Your password reset Url is: \n\n
+        ${resetPasswordUrl}
+        \n\n
+        If you have not made this request, please change your password! because your account might be at risk.
+    `;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: `My Ecommerce Password Recovery`,
+            message
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `Password recovery email sent to ${user.email}`
+        });
+
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+        await user.save();
+
+    }
+
 })
